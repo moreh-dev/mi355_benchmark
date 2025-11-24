645deaf74af868af44ea340a7156b2922f26c4dc
moi-gwangho <gwangho.choi@moreh.io>
2025-11-24 06:39:27 +0000

    update: change attn environment variable

--- team_ce/dsr1/dsr1_fp8_mi355x_docker.sh
+++ team_ce/dsr1/dsr1_fp8_mi355x_docker.sh
@@ -17,9 +17,7 @@ tensor_parallel_size=8
 
 export VLLM_SERVER_DEV_MODE=1
 export VLLM_ROCM_USE_AITER=1
-export VLLM_USE_AITER_UNIFIED_ATTENTION=1
-export VLLM_ROCM_USE_AITER_MHA=0
-export VLLM_SERVER_DEV_MODE=1
+export VLLM_SERVER_DEV_MODE=ROCM_AITER_MLA
 
 set -x
 vllm serve ${model} \
